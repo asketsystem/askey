5a171706149bb542fef31ba1d23542c9f34ae40a
asketsystem <asketsystem1@gmail.com>
2021-12-16 13:46:20 +0000

    second contract that is more practical. This contact will allow you to store the MD5 hash

---
+++ contracts/MD5SmartContrac.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract MD5SmartContrac {
+  bytes32 public signature;
+  event signEvent(bytes32 signature);
+  constructor() public {
+  }
+  function sign(string memory document) public {
+    signature = sha256(bytes(document));
+    emit signEvent(signature);
+  }
+}
