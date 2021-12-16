6da846365958a19da6b2262d354ea2e44e28c6de
asketsystem <asketsystem1@gmail.com>
2021-12-16 13:45:33 +0000

    First Contract

---
+++ contracts/BocContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract BocContract {
+  string greeting;
+  constructor() public {
+    greeting = 'Hello World';
+  }
+  function greet() public view returns (string memory) {
+    return greeting;
+  }
+}
